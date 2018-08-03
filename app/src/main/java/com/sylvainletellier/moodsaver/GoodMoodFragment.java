e41d158e1ab5c91d21d967e5a9cbdc201a9867ce
SylvainLETELLIER <contact@letellier-sylvain.fr>
2018-08-03 16:09:45 +0200

    -add smileys and icons to git -in goodMood fragment, setOnClickListener on comment icon with a toast.

--- app/src/main/java/com/sylvainletellier/moodsaver/GoodMoodFragment.java
+++ app/src/main/java/com/sylvainletellier/moodsaver/GoodMoodFragment.java
@@ -6,13 +6,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class GoodMoodFragment extends Fragment {
+
+    private ImageView mComment;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_good_mood, container, false);
+
+        mComment = v.findViewById(R.id.good_comment);
+
+        mComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(),"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_good_mood, container, false);
+        return v;
+
+
+
+
     }
 }
